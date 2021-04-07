50d20c9a005ef36cf4f2706a6c9dca5ed79c4b66
Kevin Leung <kevinresol@gmail.com>
2021-04-07 17:02:04 +0800

    Allow publishing to multiple topics

--- src/why/pubsub/mqtt/Publisher.hx
+++ src/why/pubsub/mqtt/Publisher.hx
@@ -18,18 +18,17 @@ class Publisher<Message> implements why.pubsub.Publisher<Message> {
 	}
 		
 	public function publish(message:Message):Promise<Noise> {
-		return client.publish(new why.mqtt.Message(
-			config.topic(message),
-			config.serialize(message),
-			switch config.qos {
-				case null: null;
-				case f: f(message);
-			},
-			switch config.retain {
-				case null: null;
-				case f: f(message);
-			}
-		));
+		final topics = config.topic(message);
+		final payload = config.serialize(message);
+		final qos = switch config.qos {
+			case null: null;
+			case f: f(message);
+		}
+		final retain = switch config.retain {
+			case null: null;
+			case f: f(message);
+		}
+		return Promise.inParallel([for(topic in topics) client.publish(new why.mqtt.Message(topic, payload, qos, retain))]);
 	}
 }
 
@@ -42,13 +41,21 @@ typedef PublisherConfig<Message> = {
 
 
 @:callable
-private abstract TopicConfig<Message>(Message->Topic) from Message->Topic to Message->Topic {
+private abstract TopicConfig<Message>(Message->Array<Topic>) from Message->Array<Topic> to Message->Array<Topic> {
 	@:from
-	public static inline function ofConst<Message>(v:Topic):TopicConfig<Message> {
+	public static inline function ofSingle<Message>(v:Topic):TopicConfig<Message> {
+		return _ -> [v];
+	}
+	@:from
+	public static inline function ofConst<Message>(v:Array<Topic>):TopicConfig<Message> {
 		return _ -> v;
 	}
 	@:from
 	public static inline function ofString<Message>(v:String):TopicConfig<Message> {
-		return _ -> (v:Topic);
+		return _ -> [(v:Topic)];
+	}
+	@:from
+	public static inline function ofStringArray<Message>(v:Array<String>):TopicConfig<Message> {
+		return _ -> v.map(v -> (v:Topic));
 	}
 }
